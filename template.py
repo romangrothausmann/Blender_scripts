361f198b8da2a9de101e5cbd10afdd9c79f46472
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2015-08-04 12:41:42 +0200

    modified template.py

--- template.py
+++ template.py
@@ -1,75 +1,6 @@
 ## from: https://developer.blender.org/diffusion/B/browse/master/release/scripts/templates_py/background_job.py
-# This script is an example of how you can run blender from the command line
-# (in background mode with no interface) to automate tasks, in this example it
-# creates a text object, camera and light, then renders and/or saves it.
-# This example also shows how you can parse command line options to scripts.
-#
-# Example usage for this test.
-#  blender --background --factory-startup --python $HOME/background_job.py -- \
-#          --text="Hello World" \
-#          --render="/tmp/hello" \
-#          --save="/tmp/hello.blend"
-#
-# Notice:
-# '--factory-startup' is used to avoid the user default settings from
-#                     interfearing with automated scene generation.
-#
-# '--' causes blender to ignore all following arguments so python can use them.
-#
-# See blender --help for details.
 
 import bpy
-
-
-def example_function(text, save_path, render_path):
-
-    scene = bpy.context.scene
-
-    # Clear existing objects.
-    scene.camera = None
-    for obj in scene.objects:
-        scene.objects.unlink(obj)
-
-    txt_data = bpy.data.curves.new(name="MyText", type='FONT')
-
-    # Text Object
-    txt_ob = bpy.data.objects.new(name="MyText", object_data=txt_data)
-    scene.objects.link(txt_ob)  # add the data to the scene as an object
-    txt_data.body = text        # the body text to the command line arg given
-    txt_data.align = 'CENTER'   # center text
-
-    # Camera
-    cam_data = bpy.data.cameras.new("MyCam")
-    cam_ob = bpy.data.objects.new(name="MyCam", object_data=cam_data)
-    scene.objects.link(cam_ob)  # instance the camera object in the scene
-    scene.camera = cam_ob       # set the active camera
-    cam_ob.location = 0.0, 0.0, 10.0
-
-    # Lamp
-    lamp_data = bpy.data.lamps.new("MyLamp", 'POINT')
-    lamp_ob = bpy.data.objects.new(name="MyCam", object_data=lamp_data)
-    scene.objects.link(lamp_ob)
-    lamp_ob.location = 2.0, 2.0, 5.0
-
-    if save_path:
-        try:
-            f = open(save_path, 'w')
-            f.close()
-            ok = True
-        except:
-            print("Cannot save to path %r" % save_path)
-
-            import traceback
-            traceback.print_exc()
-
-        if ok:
-            bpy.ops.wm.save_as_mainfile(filepath=save_path)
-
-    if render_path:
-        render = scene.render
-        render.use_file_extension = True
-        render.filepath = render_path
-        bpy.ops.render.render(write_still=True)
 
 
 def main():
@@ -86,21 +17,13 @@ def main():
         argv = argv[argv.index("--") + 1:]  # get all args after "--"
 
     # When --help or no args are given, print this help
-    usage_text = \
-    "Run blender in background mode with this script:"
-    "  blender --background --python " + __file__ + " -- [options]"
+    usage_text =  "Run blender in background mode with this script:"
+    usage_text += "  blender -b ~/blender/default.blend -P " + __file__ + " -- [options]"
 
     parser = argparse.ArgumentParser(description=usage_text)
 
-    # Example utility, add some text and renders or saves it (with options)
-    # Possible types are: string, int, long, choice, float and complex.
-    parser.add_argument("-t", "--text", dest="text", type=str, required=True,
-            help="This text will be used to render an image")
-
-    parser.add_argument("-s", "--save", dest="save_path", metavar='FILE',
-            help="Save the generated file to the specified path")
-    parser.add_argument("-r", "--render", dest="render_path", metavar='FILE',
-            help="Render an image to the specified path")
+    parser.add_argument("-i", "--input", dest="input", metavar='FILE', required=True, help="Input path contained in a text file.")
+    parser.add_argument("-o", "--output", dest="output", metavar='FILE', required=True, help="Output file to save the blender curve in. Suffix sets the format: Blender (.blend); DXF (.dxf); STL (.stl); Videoscape (.obj); VRML 1.0 (.wrl)")
 
     args = parser.parse_args(argv)  # In this example we wont use the args
 
@@ -108,15 +31,42 @@ def main():
         parser.print_help()
         return
 
-    if not args.text:
-        print("Error: --text=\"some string\" argument not given, aborting.")
-        parser.print_help()
-        return
+    if not args.input:
+       print('Need an input file')
+       parser.print_help()
+       sys.exit(1)
 
-    # Run the example function
-    example_function(args.text, args.save_path, args.render_path)
+    if not args.output:
+       print('Need an output file')
+       parser.print_help()
+       sys.exit(1)
 
-    print("batch job finished, exiting")
+
+
+    # Clear existing objects.
+    scene = bpy.context.scene
+    scene.camera = None
+    for obj in scene.objects:
+        scene.objects.unlink(obj)
+
+
+
+
+
+    if args.output:
+        try:
+            f = open(args.output, 'w')
+            f.close()
+            ok = True
+        except:
+            print("Cannot save to path %r" % save_path)
+
+            import traceback
+            traceback.print_exc()
+
+        if ok:
+            bpy.ops.wm.save_as_mainfile(filepath=args.output)
+
 
 
 if __name__ == "__main__":
